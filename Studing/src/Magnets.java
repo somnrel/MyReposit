71389a2b9061385779d5ff45e6496c5ce2931259
Миша <misha.k4726@gmail.com>
2019-12-07 16:33:15 +0700

    Задача Магниты

--- Studing/src/Magnets.java
+++ Studing/src/Magnets.java
@@ -6,6 +6,7 @@ public class Magnets {
 		if (x>2) {
 			System.out.print("a");
 		}
+<<<<<<< HEAD
 		while (x>0) { //повторять пока x>0
 			x =x-1; // отнимаем при каждом цикле x-1
 			System.out.print("-"); // выводи "-" после того, как отнимешь х-1
@@ -15,6 +16,17 @@ public class Magnets {
 			if (x==1) { // если x равен 1
 				System.out.print("d"); // => то выведи d
 				x = x - 1; // т.к x сейчас равен 1, то x - 1 = 0, для окончания цикла
+=======
+		while (x>0) {
+			x =x-1;
+			System.out.print("-");
+			if (x==2) {
+				System.out.print("b c");
+			}
+			if (x==1) {
+				System.out.print("d");
+				x = x - 1;
+>>>>>>> 134286b... Р—Р°РґР°С‡Р° РњР°РіРЅРёС‚С‹
 			}
 		}
 	}
