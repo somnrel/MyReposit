8e17e14c8499eb2f2618a0fd0f27d41b061a66ae
Миша <misha.k4726@gmail.com>
2019-12-08 23:41:23 +0700

    ООП задача Dog

---
+++ Studing/src/Dog/testDriveDog.java
@@ -0,0 +1,13 @@
+package Dog;
+
+public class testDriveDog {
+
+	public static void main(String[] args) {
+		Dog d = new Dog();
+		d.size = 40;
+		d.name = "John";
+		d.breed = "Labrador";
+		d.bark();
+	}
+
+}
